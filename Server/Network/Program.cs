cffbf92297135af3df1383e2797c0d66cf04a05e
CorentinO <cocoog77@gmail.com>
2023-04-13 11:33:59 +0200

    Server

---
+++ Server/Network/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Sockets;
+using NetworkClient;
+
+var client = new TcpSampleClient();
+await client.Run();
+
+namespace NetworkClient
+{
+    internal class TcpSampleClient
+    {
+        public async Task Run()
+        {
+            using var client = new TcpClient();
+            await client.ConnectAsync("10.51.0.177", 666);
+
+            using var stream = client.GetStream();
+            using var reader = new StreamReader(stream, leaveOpen: true);
+            using var writer = new StreamWriter(stream, leaveOpen: true);
+
+            async Task DisplayLines()
+            {
+                var newLine = await reader.ReadLineAsync();
+                while(newLine != null)
+                {
+                    Console.WriteLine(newLine);
+                    newLine = await reader.ReadLineAsync();
+                }
+            }
+
+            _=DisplayLines();
+            Console.WriteLine("Type a line to send to the server");
+            while (true)
+            {
+                var lineToSend = Console.ReadLine();
+
+                writer.WriteLine(lineToSend);
+                await writer.FlushAsync();
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Sockets;
+using NetworkServer;
+
+var server = new TcpSampleServer();
+await server.Run();
+
+namespace NetworkServer
+{
+    internal class TcpSampleServer
+    {        
+        private Dictionary<TcpClient, StreamWriter> _clients = new ();
+
+        public async Task Run()
+        {
+            var server = TcpListener.Create(666);
+            server.Start();
+
+            while(true)
+            {
+                var client = await server.AcceptTcpClientAsync();
+                _ = Serve(client);
+            }
+        }
+
+        private async Task Serve(TcpClient client)
+        {    
+            try
+            {
+                using (client)
+                {
+                    using var stream = client.GetStream();
+                    using var reader = new StreamReader(stream, leaveOpen: true);
+                    using var writer = new StreamWriter(stream, leaveOpen: true);
+                    _clients.Add(client, writer);
+
+
+                    var nextLine = await reader.ReadLineAsync();
+                    while (nextLine != null)
+                    {
+                        foreach (var kvp in _clients.Where(kvp => kvp.Key != client))
+                        {
+                            kvp.Value.WriteLine(nextLine);
+                            await kvp.Value.FlushAsync();
+                        }
+
+                        nextLine = await reader.ReadLineAsync();
+                    }
+                }                
+            }    
+            catch(Exception)
+            {
+            }
+            finally
+            {
+                _clients.Remove(client);
+            }
+        }
+    }
+}
